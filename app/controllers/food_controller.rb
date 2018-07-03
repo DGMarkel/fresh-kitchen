fe05e4861389fbc3bae694bbce9fa536085d337e
DGMarkel <davidgmarkel@gmail.com>
2018-07-03 09:09:17 -0400

    refactor helper method to make usage more flexible

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -10,7 +10,6 @@ class FoodController < ApplicationController
 
   post '/users/:user_slug/foods' do
     @food = Food.create(params[:food])
-
     ####conditions for creating/associating categories to foods####
 
     #if any field is empty
@@ -21,12 +20,12 @@ class FoodController < ApplicationController
       if params[:category][:id]
         @food.update(category_id: params[:category][:id])
     #if a new category is entered and its name < 50% similar to existing category names
-      elsif !params[:category][:name].empty? && !find_similar_category_by_name_for_create
+      elsif !params[:category][:name].empty? && !find_similar_category_by_name(params[:category][:name])
         category_new = Category.create(name: params[:category][:name].downcase.capitalize, user_id: current_user.id)
         @food.update(category_id: category_new.id)
     #if a new category is entered and its name > %50 similar to existing category names
-      elsif !params[:category][:name].empty? && find_similar_category_by_name_for_create
-        @food.update(category_id: find_similar_category_by_name_for_create.id)
+      elsif !params[:category][:name].empty? && find_similar_category_by_name(params[:category][:name])
+        @food.update(category_id: find_similar_category_by_name(params[:category][:name]).id)
       end
       category = current_user.categories.find_by(id: @food.category_id)
       redirect to :"/users/#{current_user.slug}/#{category.slug}"
@@ -59,12 +58,12 @@ class FoodController < ApplicationController
 
     ####conditions for editing/associating foods categories####
     #if a new category is entered and no existing category names are >= %50 similar
-    if !params[:new][:category].empty? && !find_similar_category_by_name_for_edit
+    if !params[:new][:category].empty? && !find_similar_category_by_name(params[:new][:category])
       category_new = Category.create(name: params[:new][:category].downcase.capitalize, user_id: current_user.id)
       @food.update(category_id: category_new.id)
     #if a new category is entered and an existing category name is >= %50 similar
-    elsif !params[:new][:category].empty? && find_similar_category_by_name_for_edit
-      @food.update(category_id: find_similar_category_by_name_for_edit.id)
+    elsif !params[:new][:category].empty? && find_similar_category_by_name(params[:new][:category])
+      @food.update(category_id: find_similar_category_by_name(params[:new][:category]).id)
     end
     redirect :"/users/#{current_user.slug}/#{params[:category]}"
   end
@@ -81,14 +80,9 @@ class FoodController < ApplicationController
 
   helpers do
 
-    # for post '/users/:user_slug/foods'
-    def find_similar_category_by_name_for_create
-      current_user.categories.find {|category| category.name.similar(params[:category][:name]) >= 50}
-    end
-
-    #for post '/users/:user_slug/:category/:food'
-    def find_similar_category_by_name_for_edit
-      current_user.categories.find {|category| category.name.similar(params[:new][:category]) >= 50}
+    #finds user existing user categories >= %50 similar to new category entry
+    def find_similar_category_by_name(*args)
+      current_user.categories.find {|category| category.name.similar(*args) >= 50}
     end
 
   end
