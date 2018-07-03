7e49569b899c514466ce55fa7a5f4fd37fad3687
DGMarkel <davidgmarkel@gmail.com>
2018-07-03 01:29:56 -0400

    Add comments to edit route, edit #current_user

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -9,16 +9,21 @@ class FoodController < ApplicationController
   end
 
   post '/users/:user_slug/foods' do
-
     @food = Food.create(params[:food])
 
+    ####conditions for creating/associating categories to foods####
+
+    #if any field is empty
     if params[:food].any? {|k, v| v == ""}
       redirect :"/users/#{current_user.slug}/foods/new"
     else
+    #if an existing category is selected
       if params[:category][:id]
         @food.update(category_id: params[:category][:id])
+    #if a new category is entered and its name < 50% similar to existing category names
       elsif !params[:category][:name].empty? && !find_similar_category_by_name_for_create
         category_new = Category.create(name: params[:category][:name].downcase.capitalize, user_id: current_user.id)
+    #if a new category is entered and its name > %50 similar to existing category names
       elsif !params[:category][:name].empty? && find_similar_category_by_name_for_create
         @food.update(category_id: find_similar_category_by_name_for_create.id)
       end
@@ -51,9 +56,12 @@ class FoodController < ApplicationController
     @food = current_user.foods.find_by_slug(params[:food])
     @food.update(params[:update])
 
+    ####conditions for editing/associating foods categories####
+    #if a new category is entered and no existing category names are >= %50 similar
     if !params[:new][:category].empty? && !find_similar_category_by_name_for_edit
       category_new = Category.create(name: params[:new][:category].downcase.capitalize, user_id: current_user.id)
       @food.update(category_id: category_new.id)
+    #if a new category is entered and an existing category name is >= %50 similar
     elsif !params[:new][:category].empty? && find_similar_category_by_name_for_edit
       @food.update(category_id: find_similar_category_by_name_for_edit.id)
     end
